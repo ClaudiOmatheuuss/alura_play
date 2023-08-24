b6d805288b2d9906220357538369259b1fb16916
Claud10matheus <claudiom.pst@gmail.com>
2023-08-23 23:21:10 -0300

    fifth commit: POST request

--- js/conectaApi.js
+++ js/conectaApi.js
@@ -5,6 +5,24 @@ async function listaVideos(){ // função assíncrona
     return conexaoConvertida // retorna objeto json
 }
 
+async function criaVideo(titulo, descricao, url, imagem){
+    const conexao = await fetch("http://www.localhost:3000/videos" , {
+        method: "POST" , //define metodo de requisição como metodo POST
+        Headers: {
+            "Content-type" : "application/json"       //especifica tipo de arquivo que está sendo enviado ou recebido  
+        },
+        body: JSON.stringify({ //envia objeto de valores json em forma de string
+            titulo: titulo,
+            descricao: `${descricao} mil visualizações`,    //dados que queremos cadastrar: titulo, descricao, url, imagem
+            url: url,
+            imagem: imagem
+        })
+    })
+    const conexaoConvertida = await conexao.json()
+    return conexaoConvertida
+} //agora vamos conseguir enviar valores para essa função que vao ser atribuidos a essas propriedades do body
+
 export const conectaApi = {                
-    listaVideos                                    // exporta função listaVideos()
+    listaVideos ,        
+    criaVideo                           // exporta função listaVideos()
 }
