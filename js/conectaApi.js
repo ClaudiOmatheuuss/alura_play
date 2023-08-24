96d383d5ca1403fe939291da9eeff4d3901f8cfa
Claud10matheus <claudiom.pst@gmail.com>
2023-08-24 01:21:52 -0300

    sixth commit: creating new elements and fixing bug

--- js/conectaApi.js
+++ js/conectaApi.js
@@ -1,28 +1,30 @@
-async function listaVideos(){ // função assíncrona
-    const conexao = await fetch("http://localhost:3000/videos") // espera o fetch ser resolvido e retorna uma promisse
-    const conexaoConvertida = await conexao.json() // transforma response da constante conexão em um objeto .json
+async function listaVideos() {
+    const conexao = await fetch("http://localhost:3000/videos");
+    const conexaoConvertida = await conexao.json(); // transforma response da constante conexão em um objeto .json
 
-    return conexaoConvertida // retorna objeto json
+    return conexaoConvertida;
 }
 
-async function criaVideo(titulo, descricao, url, imagem){
-    const conexao = await fetch("http://www.localhost:3000/videos" , {
-        method: "POST" , //define metodo de requisição como metodo POST
-        Headers: {
-            "Content-type" : "application/json"       //especifica tipo de arquivo que está sendo enviado ou recebido  
+async function criaVideo(titulo, descricao, url, imagem) {
+    const conexao = await fetch("http://localhost:3000/videos", {// espera o fetch ser resolvido e retorna uma promisse
+        method: "POST",//define metodo de requisição como metodo POST
+        headers: {
+            "Content-type": "application/json" //especifica tipo de arquivo que está sendo enviado ou recebido  
         },
-        body: JSON.stringify({ //envia objeto de valores json em forma de string
+        body: JSON.stringify({//envia objeto de valores json em forma de string
             titulo: titulo,
             descricao: `${descricao} mil visualizações`,    //dados que queremos cadastrar: titulo, descricao, url, imagem
             url: url,
             imagem: imagem
         })
-    })
-    const conexaoConvertida = await conexao.json()
-    return conexaoConvertida
-} //agora vamos conseguir enviar valores para essa função que vao ser atribuidos a essas propriedades do body
+    });
 
-export const conectaApi = {                
-    listaVideos ,        
-    criaVideo                           // exporta função listaVideos()
-}
+    const conexaoConvertida = conexao.json();
+
+    return conexaoConvertida;
+}//agora vamos conseguir enviar valores para essa função que vao ser atribuidos a essas propriedades do body
+
+export const conectaApi = {
+    listaVideos,
+    criaVideo
+} // exporta funções
---
+++ js/criarVideo.js
@@ -0,0 +1,16 @@
+import { conectaApi } from "./conectaApi.js";
+const formulario = document.querySelector("[data-formulario]");
+
+async function criarVideo(evento) {
+    evento.preventDefault();//evita comportamento padrão de recarregamento da página quando o evento de submit acontecer
+
+    const imagem = document.querySelector("[data-imagem]").value;//seleciona e armazena valor do input da imagem do video
+    const url = document.querySelector("[data-url]").value;//seleciona e armazena valor do input da url do video
+    const titulo = document.querySelector("[data-titulo]").value;//seleciona e armazena valor do input do titulo do video
+    const descricao = Math.floor(Math.random() * 10).toString();//gera número inteiro aleatório em formato de string para a descrição do vídeo
+
+    await conectaApi.criaVideo(titulo, descricao, url, imagem);
+    window.location.href = "../pages/envio-concluido.html";
+}
+
+formulario.addEventListener("submit", evento => criarVideo(evento)) // adciona escutador de evento que vai executar a função criarVideo com o evento como parametro quando o mesmo acontecer
