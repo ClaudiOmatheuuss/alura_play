390938e9b06bab91d1e1e4cc63538a32d4fffaa7
Claud10matheus <claudiom.pst@gmail.com>
2023-08-24 12:20:59 -0300

    eighth commit: filtering search result

--- js/buscarVideo.js
+++ js/buscarVideo.js
@@ -1,13 +1,20 @@
 import { conectaApi } from "./conectaApi.js"
+import constroiCard from "./mostrarVideos.js" //importa funcao constroiCard
 
 async function buscarVideo(evento){
     evento.preventDefault()
     const dadosDePesquisa = document.querySelector("[data-pesquisa]").value //seleciona valor do input
     const busca = await conectaApi.buscaVideo(dadosDePesquisa) //espera promisse da funcao buscaVideo ser resolvida
+    
+    const lista = document.querySelector("[data-lista]") //seleciona data-atribute referente a nossa ul de vídeos
+    
+    while (lista.firstChild){ //enquanto a lista tiver o primeiro filho
+        lista.removeChild(lista.firstChild) //remover seu primeiro filho
+    }    
 
-    console.log(busca) //imprime resultado no console
+    busca.forEach(elemento => lista.appendChild( //para cada elemento de busca chama a funcao constroiCard com seus atributos
+        constroiCard(elemento.titulo, elemento.descricao, elemento.url, elemento.imagem)))
 }
 
 const botaoDePesquisa = document.querySelector("[data-botao-pesquisa]") //seleciona botao de pesquisa
-
-botaoDePesquisa.addEventListener("click" , evento => buscarVideo(evento))
+botaoDePesquisa.addEventListener("click" , evento => buscarVideo(evento)) //ouvinte de evento
--- js/mostrarVideos.js
+++ js/mostrarVideos.js
@@ -2,7 +2,7 @@ import { conectaApi } from "./conectaApi.js"; //importa constante conectaApi que
 
 const lista = document.querySelector("[data-lista]"); //seleciona nosso data-atributte
 
-function constroiCard(titulo, descricao, url, imagem) {
+export default function constroiCard(titulo, descricao, url, imagem) {
     const video = document.createElement("li");         //atribui criação de elemento list-item
     video.className = "videos__item"; // atribui a essa constante a seguinte classe
     video.innerHTML = `<iframe width="100%" height="72%" src="${url}"
