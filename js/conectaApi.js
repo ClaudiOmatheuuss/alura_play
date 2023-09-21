7354872638d1a61598862191a2aabb8e4d59a696
Claud10matheus <claudiom.pst@gmail.com>
2023-09-21 13:33:41 -0300

    fazendo requisicao usando api externa

--- js/conectaApi.js
+++ js/conectaApi.js
@@ -1,12 +1,12 @@
 async function listaVideos() {
-    const conexao = await fetch("http://localhost:3000/videos");
+    const conexao = await fetch("https://my-json-server.typicode.com/cawndjhs/videos-api/videos");
     const conexaoConvertida = await conexao.json(); // transforma response da constante conexão em um objeto .json
 
     return conexaoConvertida;
 }
 
 async function criaVideo(titulo, descricao, url, imagem) {
-    const conexao = await fetch("http://localhost:3000/videos", {// espera o fetch ser resolvido e retorna uma promisse
+    const conexao = await fetch("https://my-json-server.typicode.com/cawndjhs/videos-api/videos", {// espera o fetch ser resolvido e retorna uma promisse
         method: "POST",//define metodo de requisição como metodo POST
         headers: {
             "Content-type": "application/json" //especifica tipo de arquivo que está sendo enviado ou recebido  
@@ -27,7 +27,7 @@ async function criaVideo(titulo, descricao, url, imagem) {
 }//agora vamos conseguir enviar valores para essa função que vao ser atribuidos a essas propriedades do body
 
 async function buscaVideo(termoDeBusca){ //faz resquisição get para obter elemento(s) na API de acordo com o valor de termoDeBusca
-    const conexao = await fetch(` http://localhost:3000/videos?q=${termoDeBusca} `)
+    const conexao = await fetch(` https://my-json-server.typicode.com/cawndjhs/videos-api/videos?q=${termoDeBusca} `)
     const conexaoConvertida = conexao.json()
 
     return conexaoConvertida
